812f718b8ae23a47184bc63eae2f3a3f3e4e7f12
Minirazs <ruishan.foo@gmail.com>
2020-12-07 13:11:12 +0000

    created toys index template

--- ToysProject/urls.py
+++ ToysProject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import toys.views
+import reviews.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('toys/', toys.views.index)
+    path('toys/', toys.views.index),
+    path('reviews/', reviews.views.index)
 ]
