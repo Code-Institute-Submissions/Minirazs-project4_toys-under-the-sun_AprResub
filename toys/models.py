e0536c5fe25003c010f039c97119a13bf75e4b15
Minirazs <ruishan.foo@gmail.com>
2020-12-07 14:55:11 +0000

    created a Toy model

--- toys/models.py
+++ toys/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Toy(models.Model):
+    title = models.CharField(blank=False, max_length=255)
+    price = models.IntegerField(blank=False)
+    desc = models.TextField(blank=False)
+
+#show the name in the database, make it into a string
+    def __str__(self):
+        return self.title 
