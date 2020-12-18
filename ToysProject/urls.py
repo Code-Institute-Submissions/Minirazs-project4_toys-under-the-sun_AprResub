c15f84ff4a255a711774680d01fe823467fd2785
Minirazs <ruishan.foo@gmail.com>
2020-12-18 07:16:03 +0000

    updated AllAuth

--- ToysProject/urls.py
+++ ToysProject/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import toys.views
 import reviews.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('toys/', toys.views.index,
          name='show_toy_route'),
     path('toys/create', toys.views.create_toy),
