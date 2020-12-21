468556b7a756f4fdb10845863147a2f6f02997fa
Minirazs <ruishan.foo@gmail.com>
2020-12-21 12:38:40 +0000

    edited create url

--- toy/urls.py
+++ toy/urls.py
@@ -4,7 +4,7 @@ import toy.views
 urlpatterns = [
     path('', toy.views.index,
          name='show_toy_route'),
-    path('create', toy.views.create_toy,
+    path('create/', toy.views.create_toy,
          name='create_toy_route'),
     path('update/<toy_id>', toy.views.update_toy,
          name='update_toy_route'),
