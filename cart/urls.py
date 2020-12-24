afe8e3d8cfb1cfc97bb4975c9c2c9252d94fc7ef
Minirazs <ruishan.foo@gmail.com>
2020-12-24 08:19:34 +0000

    updating update_qty function in views.py

--- cart/urls.py
+++ cart/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('view_cart', cart.views.view_cart,
          name='view_cart_route'),
     path('remove/<toy_id>', cart.views.remove_from_cart,
-         name='remove_from_cart')
+         name='remove_from_cart'),
+    path('update_quantity/<toy_id>',
+         cart.views.update_quantity,
+         name='update_cart_quantity_route')
 ]
