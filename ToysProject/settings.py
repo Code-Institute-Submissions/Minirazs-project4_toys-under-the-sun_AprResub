a40fea0b5b66730297e8310ecbdda03f4a3e21de
Minirazs <ruishan.foo@gmail.com>
2020-12-28 02:59:59 +0000

    editing the settings.py to move secret key to env file

--- ToysProject/settings.py
+++ ToysProject/settings.py
@@ -23,7 +23,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!e_smdr9tpx(ciahjo2m)3#31d!&0y(kau9!3-7+@atpz_exqc'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
