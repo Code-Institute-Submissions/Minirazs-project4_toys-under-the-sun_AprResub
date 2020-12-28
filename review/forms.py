5b41e068ebc09e7ba8fbc753434d860416249ee8
Minirazs <ruishan.foo@gmail.com>
2020-12-28 12:35:34 +0000

    editing the styling of Review and add Footer

--- review/forms.py
+++ review/forms.py
@@ -6,3 +6,10 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ('title', 'rating', 'review')
+        widgets = {
+            'review': forms.Textarea(attrs={
+                'rows': '4',
+                'cols': '90',
+                'maxlength': '200',
+            }),
+        }
